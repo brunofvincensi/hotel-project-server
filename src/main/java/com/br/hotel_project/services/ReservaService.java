58c6841d7dec5164799852f012e0dab360a3e307
Bruno Ferrari <brunofvincensi@gmail.com>
2022-02-06 20:36:16 -0300

    explicacao de algumas funcoes no codigo

--- src/main/java/com/br/hotel_project/services/ReservaService.java
+++ src/main/java/com/br/hotel_project/services/ReservaService.java
@@ -6,9 +6,12 @@ import com.br.hotel_project.rest.dtos.insert.HospedagemInsertDTO;
 
 public interface ReservaService {
 
+    //faz o check in da hospedagem
     Hospedagem checkIn(HospedagemInsertDTO hospedagemDTO);
 
+    //faz o check out da hospedagem calculando o valor gasto
     Hospedagem checkOut(Integer id);
 
+    //faz o pagamento da hospedagem e se for pago após o dia do check-out será cobrado uma multa
     PagamentoDTO pay(Integer hospedagemId);
 }
